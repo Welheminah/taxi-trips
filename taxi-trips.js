96f37f0751537cf68dfa2f3ce057247df826592c
Welheminah <ponelehutso@gmail.com>
2021-11-22 17:04:13 +0200

    last commit of the day

--- taxi-trips.js
+++ taxi-trips.js
@@ -13,16 +13,45 @@ module.exports = function TaxiTrips(pool) {
     }
 
     async function findTaxisForRegion(){
+        const results = await pool.query("SELECT reg_number FROM taxi WHERE reg_number")
 
     }
 
-    async function findTripsByRegNumber(){
+    async function findTripsByRegNumber(reg_number){
+        const results = await pool.query("SELECT * FROM trip WHERE reg_number = $1", [reg_number]);
+        return results.rows;
+    }
+
+    async function findTripsByRegion(){
 
     }
+
+    async function findIncomeByRegNumber(){
+
+    }
+
+    async function findTotalIncomePerTaxi(){
+
+    }
+
+    async function findTotalIncome(){
+
+    }
+
+    async function findTotalIncomeByRegion(){
+
+    }
+
     return {
         toatlTripCount,
         findAllRegions,
         findTaxisForRegion,
-        findTripsByRegNumber
+        findTripsByRegNumber,
+        findTripsByRegion,
+        findIncomeByRegNumber,
+        findTotalIncomePerTaxi,
+        findTotalIncomeByRegion,
+        findTotalIncome
+
     }
 }
